267143033fbb363ed6493fd74aa7fe312f288222
TheKeems <108583235+TheKeems@users.noreply.github.com>
2026-08-03 17:33:37 -0400

    Add files via upload

--- send.js
+++ send.js
@@ -7,7 +7,7 @@ const dataToSend = {
 
 function send(){
     console.log('sent');
-    fetch('https://indexsite.onrender.com', {
+    fetch('https://indexsite.onrender.com/api/v1/user/data', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
