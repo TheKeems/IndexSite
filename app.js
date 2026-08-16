3aafdcf94401d50b1c8e437f461a9b65261670a9
TheKeems <108583235+TheKeems@users.noreply.github.com>
2026-08-16 12:42:38 -0400

    Replace MongoDB client with Mongoose

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // /const { createServer } = require('node:http');
-const { MongoClient, ObjectId } = require('mongodb');
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const app = express();
