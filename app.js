1902d20f60d0e350f48eadef399236002327283a
TheKeems <108583235+TheKeems@users.noreply.github.com>
2026-08-16 12:48:27 -0400

    Rename prescriptData to SensorData in app.js

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ const prescriptSchema = new mongoose.Schema({
   prescript: {type: String}
 });
 
-const prescriptData = mongoose.model('SensorData', prescriptSchema);
+const SensorData = mongoose.model('SensorData', prescriptSchema);
 
 app.post('/api/data', (req, res) => {
     const {device_id, prescript} = req.body;
     const receivedData = req.body;
     console.log('Data received from client:', receivedData);
 
-    await prescriptData.findByIdAndUpdate(
+    await SensorData.findByIdAndUpdate(
         device_id, 
         {prescript}, 
         { upsert: true, new: true }
