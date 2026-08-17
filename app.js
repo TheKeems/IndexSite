136b1302f01b15975a62e8953c2cc4447eba4279
TheKeems <108583235+TheKeems@users.noreply.github.com>
2026-08-17 15:38:19 -0400

    Refactor API data endpoint with database connection

--- app.js
+++ app.js
@@ -14,25 +14,24 @@ const prescriptSchema = new mongoose.Schema({
   device_id: {type: String, required: true},
   prescript: {type: String}
 });
-
-const prescriptData = mongoose.model('SensorData', prescriptSchema);
-app.post('/api/data', async (req, res) => {
-    const {device_id, prescript} = req.body;
-    console.log('Data received from client:', device_id);
-  
-    await prescriptData.findByIdAndUpdate(
-        device_id, 
-        {prescript}, 
-        { upsert: true, new: true }
-    );
+connectToDatabase().then(() => {
+  app.post('/api/data', async (req, res) => {
+      const {device_id, prescript} = req.body;
+      console.log('Data received from client:', device_id);
+      await prescriptData.findByIdAndUpdate(
+          device_id, 
+          {prescript}, 
+          { upsert: true, new: true }
+      );
     
-    res.status(200).send({ status: 'saved' });
+      res.status(200).send({ status: 'saved' });
       /*res.status(200).json({ 
           message: 'Data received successfully!', 
           yourData: receivedData 
       });*/
-});
+  });
   
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 });
